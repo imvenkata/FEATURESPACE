9bc734a7d74c271958edb48c29cd24589d50cffa
Venkata <venkata.medabala@gfk.com>
2024-04-07 20:19:59 +0100

    setup as python package

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup()
+
+
+# setup(
+#     name="fraudmodel",  # Package name
+#     version="1.0.0",
+#     description="Detect fraudulant transactions",
+#     author="venkata",
+#     author_email="",
+#     packages=find_packages(where="src"),  # Find packages within the 'src' directory
+#     install_requires=[  # List dependencies from requirements.txt
+#         line.strip() for line in open("requirements.txt")
+#     ],
+# )
